0b15c10a7114d25daeb5b025e635c7367806ad02
SYlence_L <markyan2307@gmail.com>
2026-04-07 22:40:00 +0300

    Removed fake logo ,changed gap between navButtons

--- frontend-core/src/components/Navbar/Navbar.tsx
+++ frontend-core/src/components/Navbar/Navbar.tsx
@@ -10,10 +10,8 @@ export default function Navbar() {
                     {/* Logo */}
                     <div className="shrink-0  flex  items-center">
                         <Link to="/" className="flex items-center gap-2 group">
-                            <div className="bg-primary-500 text-white p-2 rounded-xl group-hover:bg-primary-600 group-hover:shadow-lg transition-all duration-500 shadow-md">
-                                <Tag className="w-6 h-6" />
-                            </div>
-                            <span className="font-bold text-3xl tracking-tight animated-gradient">
+
+                            <span className="font-bold  text-3xl tracking-tight animated-gradient">
                                 Sales <span className="text-zinc-800 dark:text-zinc-300">Hub</span>
                             </span>
                         </Link>
@@ -22,7 +20,7 @@ export default function Navbar() {
                     <div className="flex items-center gap-6">
                         <nav className="flex items-center">
                             {/* Center Navigation - Desktop */}
-                            <div className="hidden md:flex items-center gap-8">
+                            <div className="hidden md:flex items-center gap-4">
                                 <Link to="/" className="nav-button">
                                     Home
                                 </Link>
