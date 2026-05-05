ec1de031a7d7e0b4569877726e0617aa888cab08
SYlence_L <markyan2307@gmail.com>
2026-05-05 14:50:20 +0300

    minor change

--- frontend-core/src/App.tsx
+++ frontend-core/src/App.tsx
@@ -11,7 +11,7 @@ import Map from './pages/Map';
 
 function App() {
   const location = useLocation();
-    // Перевіряємо, чи ми зараз на сторінці карти
+  // Перевіряємо, чи ми зараз на сторінці карти
   const isMapPage = location.pathname === '/map';
   return (
     <div className="min-h-screen flex flex-col">
@@ -23,29 +23,19 @@ function App() {
           <Route path="/offers" element={<OfferPage />} />
           <Route path="/offers/create" element={<OfferCreatePage />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/map" element={<Map />} />
+
         </Routes>
       </main>
 
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Navbar />
-
-            <main className="grow pt-15 flex flex-col">
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/offers" element={<OfferPage />} />
-                    <Route path="/map" element={<Map />} />
-                </Routes>
-            </main>
-
-            {/* Показуємо Footer тільки якщо ми НЕ на карті */}
-            {!isMapPage && (
-                <footer className="py-8 text-center text-zinc-500 dark:text-zinc-400 text-sm mt-auto border-t border-zinc-200 dark:border-zinc-800">
-                    <p>&copy; {new Date().getFullYear()} SalesHub. All rights reserved.</p>
-                </footer>
-            )}
-        </div>
-    );
+      {/* Показуємо Footer тільки якщо ми НЕ на карті */}
+      {!isMapPage && (
+        <footer className="py-8 text-center text-zinc-500 dark:text-zinc-400 text-sm mt-auto border-t border-zinc-200 dark:border-zinc-800">
+          <p>&copy; {new Date().getFullYear()} SalesHub. All rights reserved.</p>
+        </footer>
+      )}
+    </div>
+  );
 }
 
 export default App;
