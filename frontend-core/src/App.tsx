4885365fd8c5eee712d7bda057aeac1ce35f05c2
SYlence_L <markyan2307@gmail.com>
2026-05-13 15:26:39 +0300

    minor change

--- frontend-core/src/App.tsx
+++ frontend-core/src/App.tsx
@@ -8,28 +8,28 @@ import OfferCreatePage from './pages/OfferCreatePage';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="grow pt-15">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/offers" element={<OfferPage />} />
-          <Route path="/offers/create" element={
-            <ProtectedRoute>
-              <OfferCreatePage />
-            </ProtectedRoute>
-          } />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </main>
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="grow pt-15">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/offers" element={<OfferPage />} />
+                    <Route path="/offers/create" element={
+                        <ProtectedRoute>
+                            <OfferCreatePage />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </main>
 
-      <footer className="py-8 text-center text-zinc-500 dark:text-zinc-400 text-sm mt-auto border-t border-zinc-200 dark:border-zinc-800">
-        <p>&copy; {new Date().getFullYear()} SalesHub. All rights reserved.</p>
-      </footer>
-    </div>
-  )
+            <footer className="py-8 text-center text-zinc-500 dark:text-zinc-400 text-sm mt-auto border-t border-zinc-200 dark:border-zinc-800">
+                <p>&copy; {new Date().getFullYear()} SalesHub. All rights reserved.</p>
+            </footer>
+        </div>
+    )
 }
 
 export default App;
