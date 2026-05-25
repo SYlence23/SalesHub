fb60d55cbc016aec78c819937d4b00277cbd3e6e
komarkrr <hrista772@gmail.com>
2026-05-25 22:51:51 +0300

    initialize backend project and configure docker-compose services

--- backend-core/Project/Project/Program.cs
+++ backend-core/Project/Project/Program.cs
@@ -83,7 +83,10 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 
 app.UseCors("AllowAll");
