cd00cfff97aacd4300afc1e1b570068c73dc7c82
adiia11 <nadiaverbova007@gmail.com>
2026-05-26 12:20:52 +0300

    Update RegisterForm.tsx

--- frontend-core/src/components/Auth/RegisterForm.tsx
+++ frontend-core/src/components/Auth/RegisterForm.tsx
@@ -76,7 +76,7 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ onSwitchMode }) => {
             )}
             {success && (
                 <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-                    <span className="block sm:inline">Реєстрація успішна! Перенаправлення...</span>
+                    <span className="block sm:inline">Реєстрація успішна!</span>
                 </div>
             )}
 
